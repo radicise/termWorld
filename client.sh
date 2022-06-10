6f6cb2423e16493f48f44239f8ee7c5ca016035f
radicise <93229379+radicise@users.noreply.github.com>
2022-06-10 16:07:13 -0400

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,4 +1,4 @@
-# stty and Java 1.8+ must be available on $PATH
+# stty and Java 1.8+ must be available
 # This script is currently only configured for demo mode;
 # `client.jar' must be launched with different arguments in order to use a different login
 HBNDFMPMHPGNIIOM=$(stty -g)
